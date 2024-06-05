c380f5a927938fc25616b3f6a105ba3ccc067953
Rayyanafie <92725940+Rayyanafie@users.noreply.github.com>
2024-06-05 21:58:14 +0700

    Update Stored Procedure Add Location

--- Stored Procedure.sql
+++ Stored Procedure.sql
@@ -27,3 +27,20 @@ BEGIN
     DELETE FROM tbl_departments
     WHERE id = @id;
 END
+--Add Location--
+CREATE PROCEDURE sp_AddLocation
+    @street_address VARCHAR(40),
+    @postal_code VARCHAR(12),
+    @city VARCHAR(30),
+    @state_province VARCHAR(25),
+    @country CHAR(3)
+AS
+BEGIN
+    INSERT INTO tbl_locations (street_address, postal_code, city, state_province, country)
+    VALUES (@street_address, @postal_code, @city, @state_province, @country);
+END
+--Update Location--
+--Delete Location--
+--Add Country--
+--Update Country--
+--Delete Country--
